dd56fb5baa6724387a7fab5bfbbe4c9f19df10f9
Iason Papadopoulos <jason099080@hotmail.com>
2018-04-04 23:07:06 +0100

    implement argument handling in main.cpp
    not finished yet

--- source/main.cpp
+++ source/main.cpp
@@ -23,29 +23,46 @@ int32_t getMsed3Error(uint32_t num, std::vector<std::vector<int32_t>> nodes) {
     return msed3s[idx];
 }
 
-int main () {
-    movable_part1 mp1;
+//people can either add an id0, or do a gpu bruteforce
+//by default this program will automatically go to the gpu bruteforce
+int main (int argc, char **argv) {
+    if (argc == 1) {
+        try {
+            movable_part1 mp1;
+            file_reading::readMP1(&mp1);
+            std::vector<std::vector<int32_t>> nodes = data_nodes::readNodes(mp1.isNew3DS);
 
-    try {
-        file_reading::readMP1(&mp1);
-        std::vector<std::vector<int32_t>> nodes = data_nodes::readNodes(mp1.isNew3DS);
+            uint32_t lfcs_num = mp1.LFCS[0] | (mp1.LFCS[1] << 8) | (mp1.LFCS[2] << 16) | (mp1.LFCS[3] << 24);
+            std::cout << "lfcs_num: " << std::hex << lfcs_num << std::endl;
 
-        uint32_t lfcs_num = mp1.LFCS[0] | (mp1.LFCS[1] << 8) | (mp1.LFCS[2] << 16) | (mp1.LFCS[3] << 24);
-        std::cout << "lfcs_num: " << std::hex << lfcs_num << std::endl;
+            int32_t msed3error = getMsed3Error(lfcs_num >> 12, nodes);
+            std::cout << "msed3error: " << std::dec << msed3error << std::endl;
 
-        int32_t msed3error = getMsed3Error(lfcs_num >> 12, nodes);
-        std::cout << "msed3error: " << std::dec << msed3error << std::endl;
+            mp1.msed3estimate = ((lfcs_num / 5) + (-1) * msed3error);
+            std::cout << "msed3estimate: " << std::hex << mp1.msed3estimate << std::endl;
 
-        mp1.msed3estimate = ((lfcs_num / 5) + (-1) * msed3error);
-        std::cout << "msed3estimate: " << std::hex << mp1.msed3estimate << std::endl;
+            launcher::doMining(mp1);
+        } catch (std::exception e) {
+            std::cout << "An exception occurred!" << std::endl;
+            std::cout << e.what() << std::endl;
+            system("pause");
+            return -1;
+        }
+    } 
+    else if (argc > 1 && (strcmp(argv[1], "id0") == 0)) {
+        if (argc < 3) {
+            std::cout << "id0 argument specified, but no id0 was provided!" << std::endl;
+            system("pause");
+            return -1;
+        }
 
-        launcher::doMining(mp1);
-
-    } catch (std::exception e) {
-        std::cout << "An exception occurred!" << std::endl;
-        std::cout << e.what() << std::endl;
+        //here we open movable_part1.sed and write to it the id0
+    } 
+    else {
+        std::cout << "Incorrect usage!" << std::endl;
+        std::cout << "Correct usage:" << std::endl << " - Seedminer" << std::endl 
+        << " - Seedminer id0 abcdef012345EXAMPLEdef0123456789" << std::endl;
         system("pause");
-        return -1;
     }
 
     return 0;
